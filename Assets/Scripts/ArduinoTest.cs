447479e9fc755aa856172a87d31ed548c8a1193d
ZigZogger <ziggycooperhenry@gmail.com>
2025-07-03 15:27:59 -0700

    DOES NOT WORK

---
+++ Assets/Scripts/ArduinoTest.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO.Ports;
+using System.Threading;
+
+public class ArduinoTest : MonoBehaviour
+{
+    Thread IOThread = new Thread(DataThread);
+    static SerialPort sp;
+
+    static string incomingMsg = "";
+    const int arduinoDelay = 200;
+
+    static int test = 0;
+
+    static void DataThread()
+    {
+        sp = new SerialPort("COM3", 9600);
+        sp.Open();
+
+        sp.ReadTimeout = 1;
+        while (true)
+        {
+            incomingMsg = sp.ReadExisting();
+
+            test++;
+            incomingMsg = "" + test;
+            Thread.Sleep(arduinoDelay);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        IOThread.Abort();
+        sp.Close();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IOThread.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(sp.ReadLine());
+        
+        /*string rawData = incomingMsg;
+        string[] cutData = rawData.Split(", ");
+
+        print("L: " + cutData[0] + ", R: " + cutData[1]);*/
+    }
+}
