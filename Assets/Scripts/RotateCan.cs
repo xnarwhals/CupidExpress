8d4e71998bebf80dba95fab6b32f7e808c85bdb9
Kai-SF <125725235+Kai-SF@users.noreply.github.com>
2025-08-14 14:49:25 -0700

    Added an alternative directional sign model, WIP connection tunnel, made a rotating can script that rotates cans.

---
+++ Assets/Scripts/RotateCan.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCan : MonoBehaviour
+{
+    [SerializeField] private Vector3 _rotation;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(_rotation * Time.deltaTime);
+    }
+}
