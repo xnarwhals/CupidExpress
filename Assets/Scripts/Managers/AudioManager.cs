69f4ebe9084803f2de7f0de675345b46a87500b7
ZigZogger <ziggycooperhenry@gmail.com>
2025-08-19 15:04:25 -0700

    environmental stuff added

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -13,6 +13,7 @@ public class AudioManager : MonoBehaviour
     public AudioSource musicSource;
     public AudioSource sfxSource;
     public AudioSource uiSource;
+    public AudioSource eternalAmbience;
 
     [Header("Race Music")]
     public AudioClip menuMusic;
@@ -26,6 +27,7 @@ public class AudioManager : MonoBehaviour
     public AudioClip raceStartSound;
     public AudioClip lapCompleteSound;
     public AudioClip hitConeSound;
+    public AudioClip environmental;
 
     [Header("Item SFX")]
     public AudioClip itemPickupSound;
@@ -73,6 +75,8 @@ public class AudioManager : MonoBehaviour
             isMenu = true;
             PlayMusic(menuMusic);
         }
+        else
+        PlayConstantAmbience();
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
@@ -341,6 +345,12 @@ public class AudioManager : MonoBehaviour
     public void PlayHitCone()
     {
         PlaySFX(hitConeSound, 0.5f);
+    }
+
+    public void PlayConstantAmbience()
+    {
+        eternalAmbience.loop = true;
+        eternalAmbience.PlayOneShot(environmental, 1.0f);
     }
 
 
